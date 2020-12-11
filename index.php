0b113717eeef9b84ec72ed4a97973dbf278f5bf3
lzampieri <zampieri.leonardo98@gmail.com>
2020-12-11 16:57:10 +0100

    Anchors2

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ require_once("utilities.php");
 <h2 class="sansserif brownbrown">ultimi bites</h2>
     <?php
     $bites = query("SELECT * FROM bites WHERE Visible = 1 ORDER BY ID DESC LIMIT 3");
-    $nl2br = 'nl2br';
+    $substr = 'substr';
     $strtolower = 'strtolower';
     while( $bite = mysqli_fetch_assoc($bites) ) {
         if( $bite['Visible'] )
@@ -23,8 +23,8 @@ require_once("utilities.php");
             <div class="row my-5">
                 <img src="fava.png" class="col-3 col-md-2 mb-auto" alt="fava" />
                 <div class="mr-auto col-md-7">
-                    <h2 class="sansserif greenbrown">{$strtolower($bite['Title'])}</h2>
-                    {$nl2br($bite['Text'])}
+                    <a href="bites.php#bite{$bite['ID']}"><h2 class="sansserif greenbrown">{$strtolower($bite['Title'])}</h2></a>
+                    {$substr($bite['Text'],0,200)}...
                 </div>
             </div>
 HTML;
